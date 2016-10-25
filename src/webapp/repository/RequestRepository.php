e6eace2030fde071b01118213c454e06c4a3fc0b
Gabriel Berthling-Hansen <gabrielbhansen@hotmail.com>
2016-10-25 18:08:11 +0200

    Add prepared statements and changed variable name to database column name
    Add prepared statements to prevent from SQL-injection
    Change name from timestamp to time to better represent database column name

--- src/webapp/repository/RequestRepository.php
+++ src/webapp/repository/RequestRepository.php
@@ -12,15 +12,25 @@ class RequestRepository
      */
     private $pdo;
 
+    private $COUNT_QUERY;
+    private $INSERT_QUERY;
+
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
+
+        $this->COUNT_QUERY = $pdo->prepare("SELECT COUNT(*) FROM request WHERE ip=:ip AND time>:time");
+        $this->INSERT_QUERY = $pdo->prepare("INSERT INTO request (ip, time) VALUES (:ip, :time)");
     }
 
-    public function countAfter($timestamp, $ip){
-        $sql  = sprintf("SELECT COUNT(*) FROM request WHERE ip='%s' AND time>$timestamp", $ip);
-        $result = $this->pdo->query($sql);
-        $row = $result->fetch();
+    public function countAfter($time, $ip){
+        $COUNT_QUERY = $this->COUNT_QUERY;
+        $COUNT_QUERY->bindParam(':ip', $ip);
+        $COUNT_QUERY->bindParam(':time', $time);
+
+        $COUNT_QUERY->execute();
+        $row = $COUNT_QUERY->fetch();
+
         if($row === false) {
             return array(0);
         }
@@ -29,13 +39,14 @@ class RequestRepository
 
     public function save(Request $request)
     {
-        $ip          = $request->getIp();
-        $time        = $request->getTime();
+        $ip = $request->getIp();
+        $time = $request->getTime();
 
-        $query = "INSERT INTO request (ip, time) "
-            . "VALUES ('$ip', '$time')";
+        $INSERT_QUERY = $this->INSERT_QUERY;
+        $INSERT_QUERY->bindParam(':ip', $ip);
+        $INSERT_QUERY->bindParam(':time', $time);
 
-        $this->pdo->exec($query);
+        $INSERT_QUERY->execute();
         return $this->pdo->lastInsertId();
     }
 }
