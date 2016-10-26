802e03805e382ae55f14f508fb833897fc989d0e
Eirik Bertelsen <xecor2k@hotmail.com>
2016-10-26 12:36:13 +0200

    fixed errors

--- src/webapp/validation/RegistrationFormValidation.php
+++ src/webapp/validation/RegistrationFormValidation.php
@@ -30,11 +30,13 @@ class RegistrationFormValidation
         if (strlen($password) < 8) {
             $this->validationErrors[] = 'Password must contain 8 characters or more';
         }
-        if (strtolower($password) = $password || strtoupper($password) = $password) {
-            $this->validationErrors[] = 'Password must contain both upper and lower case letters'
+        
+        if (strtolower($password) == $password || strtoupper($password) == $password) {
+            $this->validationErrors[] = 'Password must contain both upper and lower case letters';
         }
+
         if (!preg_match('~[0-9]~', $password)){
-            $this->validationErrors[] = 'Password must contain atleast one number'
+            $this->validationErrors[] = 'Password must contain atleast one number';
         }
 
         if (empty($password)) {
